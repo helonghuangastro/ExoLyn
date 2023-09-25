76523fe2c870f65aebcf467f85d4dd49ec9c03c0
Helong <hhl18@tsinghua.org.cn>
2023-09-25 16:58:41 +0800

    Output the atmosphere to a file

---
+++ output.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pdb
+import parameters as pars
+
+def writeatm(y, grid, filename, additional=''):
+    with open(filename, 'w') as opt:
+        # write additional
+        opt.write(additional)
+
+        # write the header
+        header = 'logP'
+        for solidname in pars.solid:
+            header += ' '
+            header += solidname + '(s)'
+        for gasname in pars.gas:
+            header += ' '
+            header += gasname
+        header += ' nuclei\n'
+        opt.write(header)
+
+        # write each row (different pressure layer)
+        for i in range(len(grid)):
+            line = str(grid[i])
+            for concentration in y[:, i]:
+                line += ' '
+                line += str(concentration)
+            line += '\n'
+            opt.write(line)
