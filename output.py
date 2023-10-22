18c5b6a60689ac7ec23b3ab1f7c46082c7e914b1
Chris Ormel <cwosci@gmail.com>
2023-10-22 15:06:28 +0800

    changed/added output.writeatm a bit...

--- output.py
+++ output.py
@@ -2,27 +2,31 @@ import numpy as np
 import pdb
 import parameters as pars
 
-def writeatm(y, grid, filename='grid.txt', additional=''):
+def writeatm(y, grid, filename='grid.txt', additional='', fmt1='{:12.4e}'):
+    """
+    chris: what is the meaning of "additional"?
+    """
+
+    print("[output]:writing output to filen: "+filename)
     with open(filename, 'w') as opt:
         # write additional
         opt.write(additional)
 
         # write the header
-        header = 'logP'
+        header = '#cols::logP'
         for solidname in pars.solid:
-            header += ' '
+            header += ','
             header += solidname + '(s)'
         for gasname in pars.gas:
-            header += ' '
+            header += ','
             header += gasname
-        header += ' nuclei\n'
+        header += ',nuclei\n'
         opt.write(header)
 
+
         # write each row (different pressure layer)
+        Nc, Ngrid = y.shape
+        fmt2 = (Nc+1)*fmt1+'\n'
         for i in range(len(grid)):
-            line = str(grid[i])
-            for concentration in y[:, i]:
-                line += ' '
-                line += str(concentration)
-            line += '\n'
+            line = fmt2.format(*((grid[i],)+tuple(y[:,i])))
             opt.write(line)
