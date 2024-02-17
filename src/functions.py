4688bf7a404476586003c76df89d1c86976e4f42
Helong <hhl18@tsinghua.org.cn>
2024-02-17 15:28:39 +0800

    Support a user-defined coagulation effeciency f_coag. Issue: f_coag cannot be 0, or initialization would fail, because the nucleation cannot be balanced by coagulation.

--- src/functions.py
+++ src/functions.py
@@ -383,7 +383,9 @@ def cal_t_coag_inv(ap, rhop, n_p, cache, deltv=0):
     Dp = cnt.kb * Tarr / (6*np.pi * eta * ap)
 
     #following OM19
-    return 2*np.pi * n_p * ap**2 * deltv + 2*np.pi * np.minimum(vBM*ap, Dp) * ap * n_p
+    t_coag_inv = 2*np.pi * n_p * ap**2 * deltv + 2*np.pi * np.minimum(vBM*ap, Dp) * ap * n_p
+    t_coag_inv *= pars.f_coag
+    return t_coag_inv
 
 def cal_Sc_all(xv, aparr, n_parr, bs, chem, cache):
     ''' To calculate the condensation rate for all of the reaction. 
