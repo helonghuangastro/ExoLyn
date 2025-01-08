b27f9e2d3156f1801e61a2ddabec74b49a2d5165
Helong <hhl18@tsinghua.org.cn>
2025-01-08 16:38:25 +0800

    Add planck constant and Stefan-Boltzmann constant to the file

--- src/constants.py
+++ src/constants.py
@@ -8,7 +8,8 @@ R_earth = 6.371e8
 mu = 1.66053904e-24    #atomic mass unit, g
 
 kb = 1.380649e-16    # Boltzmann constant, g cm^2 s^-2 K^-1
+sigma_sb = 5.670374419e-5    # Stefan-Boltzmann constant, erg cm^-2 s^-1 K^-4
 R = 8.31441        # ideal gas constant, J mol^-1 K-1
 
 c = 2.99792458e10    # speed of light, cm s^-1
-
+h = 6.62607015e-27    # Planck constant, erg * s
